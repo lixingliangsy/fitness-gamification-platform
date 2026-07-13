153e7bab1905b3cdc971dd54247cee11720e419f
lixingliangsy <154059826+lixingliangsy@users.noreply.github.com>
2026-07-14 06:58:20 +0800

    add next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  // Note
+  // Note
+  // Note
+}
+
+module.exports = nextConfig
